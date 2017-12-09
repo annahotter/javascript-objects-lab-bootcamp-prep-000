9188678396cac8b209fcf08f7dfff823ada60629
Anna Hotter <annajhotter@gmail.com>
2017-12-09 23:39:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var recipes = { prop: 1};
 
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, recipes, { prop2: 2 })
+}
+
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   recipes.prop2 = 2
   return recipes
 }
+
+
