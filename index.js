9f07e983f2aa6f4598a53c400785a78476331c2d
Anna Hotter <annajhotter@gmail.com>
2017-12-09 23:18:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  recipes["meatload"] = 'beef'
+  recipes["prop2"] = '2'
   return recipes
 }
