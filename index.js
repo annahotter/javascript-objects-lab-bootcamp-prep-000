f041c847c6531310361840a272d8cbd5145dc142
Anna Hotter <annajhotter@gmail.com>
2017-12-09 23:35:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, recipes, { prop2: 2 })
 }
 
+var recipes = { prop: 1, prop2: 2};
+
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   recipes["prop2"] = 2
   return recipes
